ababba1e0ce0e9ab221c1f89d92962411fbf2eab
Cherepakhin_PC <cherepakhin87@bk.ru>
2023-03-07 11:19:22 +0900

    Initial commit

---
+++ main.py
@@ -0,0 +1,56 @@
+from PyQt5 import QtWidgets, QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
+import sys  # We need sys so that we can pass argv to QApplication
+import os
+from random import randint
+import serial
+
+class MainWindow(QtWidgets.QMainWindow):
+
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+
+        self.graphWidget = pg.PlotWidget()
+        self.setCentralWidget(self.graphWidget)
+
+        self.x = list(range(100))  # 100 time points
+        self.y = [randint(0, 100) for _ in range(100)]  # 100 data points
+
+        self.graphWidget.setBackground('w')
+
+        pen = pg.mkPen(color=(255, 0, 0))
+        self.data_line = self.graphWidget.plot(self.x, self.y, pen=pen)
+
+        self.timer = QtCore.QTimer()
+        self.timer.setInterval(50)
+        self.timer.timeout.connect(self.update_plot_data)
+        self.timer.start()
+
+    def update_plot_data(self):
+        self.x = self.x[1:]  # Remove the first y element.
+        self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+        self.y = self.y[1:]  # Remove the first
+        self.y.append(self.get_data())  # Add a new random value.
+
+        self.data_line.setData(self.x, self.y)  # Update the data.
+
+    def get_data(self):
+        with serial.Serial('COM4', 57600, timeout=1) as ser:
+            self.line = ser.readline()
+            self.data = self.line.strip()
+            self.data = str(self.data.decode()).split(':')
+            print(self.data)
+            if len(self.data) == 4:
+                return int(self.data[3])
+            else:
+                return 0
+def main():
+    app = QtWidgets.QApplication(sys.argv)
+    main = MainWindow()
+    main.show()
+    sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    main()
