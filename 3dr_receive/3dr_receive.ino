b5945a5a0150b7ddf3ac7e62ce5ee391ab6dc225
Cherepakhin_PC <cherepakhin87@bk.ru>
2023-03-17 00:36:52 +0900

    cod

--- 3dr_receive/3dr_receive.ino
+++ 3dr_receive/3dr_receive.ino
@@ -1,5 +1,5 @@
 #include <Adafruit_ADS1X15.h>
-#include <Wire.h> 
+#include <Wire.h>
 //#include <LiquidCrystal_I2C.h>
 #include <Adafruit_MCP4725.h>
 #include <SoftwareSerial.h>
@@ -16,29 +16,31 @@ Adafruit_MCP4725 dac;
 
 
 
-
+bool stream = false;
 uint16_t i;
 const PROGMEM uint16_t DACLookup_FullSine_5Bit[32] =
 {
   2048, 2447, 2831, 3185, 3495, 3750, 3939, 4056,
   4095, 4056, 3939, 3750, 3495, 3185, 2831, 2447,
   2048, 1648, 1264,  910,  600,  345,  156,   39,
-     0,   39,  156,  345,  600,  910, 1264, 1648
+  0,   39,  156,  345,  600,  910, 1264, 1648
 };
 
 void setup(void)
 {
-//  lcd.init();                  
-//  lcd.backlight();
-//  lcd.clear();
-//  lcd.setCursor(0,0);
-//  lcd.print("Voltage Arduino");
-//  lcd.setCursor(0,1);
-//  lcd.print("AIN0: ");
- 
+  //  lcd.init();
+  //  lcd.backlight();
+  //  lcd.clear();
+  //  lcd.setCursor(0,0);
+  //  lcd.print("Voltage Arduino");
+  //  lcd.setCursor(0,1);
+  //  lcd.print("AIN0: ");
+  pinMode(13, OUTPUT);
   Serial.begin(9600);
+  Serial.setTimeout(50);
   tlmSerial.begin(57600);
-  
+  tlmSerial.setTimeout(50);
+
 
   if (!ads.begin()) {
     Serial.println("Failed to initialize ADS.");
@@ -55,29 +57,74 @@ void loop(void)
 {
   int16_t adc0;
   float volts0;
-  
+
   adc0 = ads.readADC_SingleEnded(0);
   volts0 = ads.computeVolts(adc0);
 
-  Serial.println(volts0);
- 
-//  lcd.setCursor(6,1);
-//  lcd.print(String(volts0)+"V     ");
+  //  Serial.println(volts0);
 
-  
+  //  lcd.setCursor(6,1);
+  //  lcd.print(String(volts0)+"V     ");
+
+
   dac.setVoltage(pgm_read_word(&(DACLookup_FullSine_5Bit[i])), false);
 
-  tlmSerial.println(String(volts0));
-    if (tlmSerial.available())
+  if (stream)
   {
-    char data = tlmSerial.read();
-    Serial.println(data);
+    digitalWrite(13, HIGH);
+    tlmSerial.print("#D#");
+    tlmSerial.print(int(volts0*100));
+//    tlmSerial.print(567);
+    tlmSerial.print("#");
+    tlmSerial.print(678);
+    tlmSerial.print("#");
+    tlmSerial.print(345);
+    tlmSerial.print("#");
+    tlmSerial.print(123);
+    tlmSerial.print("#");
+    tlmSerial.print(789);
+    tlmSerial.print("#");
+    tlmSerial.print(15);
+    tlmSerial.write("#\n");
   }
-  
+  else
+  {
+    digitalWrite(13, LOW);
+  }
+
+
+
+  if (tlmSerial.available())
+  {
+    //    String data = tlmSerial.readString();
+    //    Serial.println(data);
+    char data = tlmSerial.read();
+    Serial.print(data);
+    if (data == '?')
+    {
+      Serial.write(data);
+      stream = false;
+      tlmSerial.write("#!#5#\n");
+    }
+    if (data == 'A')
+    {
+      Serial.write(data);
+      stream = true;
+
+    }
+    if (data == 'S')
+    {
+      Serial.write(data);
+      stream = false;
+
+    }
+
+  }
+
   i++;
-  if (i>31) 
+  if (i > 31)
   {
     i = 0;
   }
-  delay(100);
+  delay(50);
 }
